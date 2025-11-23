ec472ff9ea98f70720a4429f2aab354013e818dc
Dan Nguyen <Dan_Nguyen1@student.uml.edu>
2025-11-23 01:13:25 -0500

    dan: add full transformer training loop

--- models/transformer.py
+++ models/transformer.py
@@ -1,25 +1,32 @@
 from datasets import Dataset
 from transformers import AutoTokenizer, AutoModelForSequenceClassification, TrainingArguments, Trainer, AutoConfig
-from sklearn.metrics import mean_squared_error, r2_score
+from sklearn.metrics import recall_score, precision_score, balanced_accuracy_score, f1_score
 from sklearn.preprocessing import MinMaxScaler
 import torch
 import pandas as pd
 import os
+import numpy as np
 
 def tokenize_function(examples, tokenizer):
     # tokenize the description
-    return tokenizer(examples['input'], padding="max_length", truncation=True, max_length=512)
+    return tokenizer(examples['text'], padding="max_length", truncation=True, max_length=512)
 
 def evaluate_model(trainer, dataset):
     ###################### UPDATE FOR OUR METRICS ###################################
     if MODEL_NUM == 1:
-        predictions = trainer.predict(dataset).predictions[0]
+        predictions = trainer.predict(dataset).predictions
     else:
         predictions = trainer.predict(dataset).predictions
-    labels = dataset['label']
-    mse = mean_squared_error(labels, predictions)
-    r2 = r2_score(labels, predictions)
-    print(f"MSE: {mse}\nR2: {r2}")
+    labels = np.array(dataset['label'])
+
+    predictions = np.argmax(predictions, axis=1)
+
+    recall = recall_score(labels, predictions, average='macro')
+    precision = precision_score(labels, predictions, average='macro')
+    balanced_accuracy = balanced_accuracy_score(labels, predictions)
+    f1 = f1_score(labels, predictions, average='macro')
+
+    print(f"recall: {recall}, precision: {precision}, balanced accuracy: {balanced_accuracy}, f1: {f1}")
 
 DEVICE = 'cuda' if torch.cuda.is_available() else 'cpu'
 
@@ -48,7 +55,16 @@ if __name__ == '__main__':
 
     df = pd.read_csv(os.path.join("..", data_dir, data_file))
 
-    target = "labels"
+    df = df.rename(columns={'input': 'text', 'labels': 'label'})
+
+    df = df[['text', 'label']]
+
+    labels = pd.read_csv(os.path.join("..", data_dir, label_file))
+    label_to_val = dict(zip([label for label in labels['labels']], labels.index.to_list()))
+
+    df['label'] = df['label'].map(label_to_val)
+
+    target = "label"
 
     dataset = Dataset.from_pandas(df)
 
@@ -68,7 +84,7 @@ if __name__ == '__main__':
     test_dataset = test_dataset.map(lambda examples: tokenize_function(examples, tokenizer), batched=True)
 
 
-    config = AutoConfig.from_pretrained(model_id, num_labels=1, ignore_mismatched_sizes=True)
+    config = AutoConfig.from_pretrained(model_id, num_labels=len(labels), ignore_mismatched_sizes=True)
 
     model = AutoModelForSequenceClassification.from_pretrained(model_id, config=config)
 
@@ -76,14 +92,14 @@ if __name__ == '__main__':
     training_args = TrainingArguments(
         output_dir='./results',
         learning_rate=1e-4,
-        num_train_epochs=8,
+        num_train_epochs=1,
         per_device_train_batch_size=8,
         per_device_eval_batch_size=8,
         warmup_steps=500,
         weight_decay=0.01,
         logging_dir='./logs',
-        evaluation_strategy="epoch",
-        eval_accumulation_steps=5 # this transfers memory from GPU to CPU, comment out if your hardware is less prohibitive
+        eval_strategy="epoch",
+        eval_accumulation_steps=5,
         # use_cpu=True
     )
 
